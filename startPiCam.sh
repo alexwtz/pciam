7e90dd22a8e97e41fb480cb85cdf95cefd2d974e
picam <alexwtz@gmail.com>
2016-05-01 13:30:04 +0000

    Ajout du mot de passe login

--- startPiCam.sh
+++ startPiCam.sh
@@ -5,6 +5,6 @@ mkdir /tmp/stream
 raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 50 -t 0 -th 0:0:0 &
 cd /home/pi/picam/
 cd mjpg-streamer-code-182/mjpg-streamer
-./mjpg_streamer  -i "./input_file.so -f /tmp/stream -n pic.jpg" -o "./output_http.so -w ./www -c pi:070288aw" &
+./mjpg_streamer  -i "./input_file.so -f /tmp/stream -n pic.jpg" -o "./output_http.so -w ./www -c admin:secret" &
 #cd ../../PiCam
 #python picam.py
